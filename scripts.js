bb5af20fd6bde1476e9f3c2946a49b4d19c204b0
abrahamsj <90579607+abrahamsj@users.noreply.github.com>
2023-12-09 11:31:31 -0500

    Adding website info
    adding code files as wells as images and links

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function toggleBookInfo(bookElement) {
+    const bookInfo = bookElement.querySelector('.book-info');
+    bookInfo.style.display = bookInfo.style.display === 'none' ? 'block' : 'none';
+}
